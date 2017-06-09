22cb71c6937d589188c84e86075aeac55817f33f
JWaller <wallerj@gmail.com>
2017-06-08 23:37:21 -0600

    added write to bucket function

--- src/get_s3_files.py
+++ src/get_s3_files.py
@@ -14,5 +14,11 @@ def retrieve_from_bucket(file):
     X = np.load(file)
     return X
 
+def write_to_bucket(bucket_name, file):
+    conn = boto.connect_s3(access_key, access_secret_key)
+    b = conn.get_bucket(bucket_name)
+    file_object = b.new_key('sample.txt')
+
 if __name__ == '__main__':
     retrieve_from_bucket('imgs_jpgs.zip')
+    write_to_bucket('lifeclef2015jen', )

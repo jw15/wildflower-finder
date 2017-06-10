66c45c4251d067d4181eb52f739cf52652554a83
JWaller <wallerj@gmail.com>
2017-06-09 21:01:42 -0600

    huzzah

--- src/get_s3_files.py
+++ src/get_s3_files.py
@@ -25,17 +25,18 @@ def retrieve_from_bucket(file):
 def write_to_bucket(bucket_name, filepath, filename):
     s3 = boto3.client('s3')
     s3.upload_file(filepath, bucket_name, filename)
-    print('Uploaded - yay!')
+    print('File uploaded - yay!')
 
 def write_folder_to_bucket(root):
     s3 = boto3.client('s3')
     files = [f for f in listdir(root) if isfile(join(root, f))]
     for thing in files:
         write_to_bucket('capstonedatajen', '{}{}'.format(root, thing), '{}'.format(thing))
-    print('Uploaded - HUZZAH!')
+    print('Folder uploaded - HUZZAH!')
 
 
 if __name__ == '__main__':
     # retrieve_from_bucket('imgs_jpgs.zip')
-    write_to_bucket('capstonedatajen', '../imgs_for_readme_rsz.zip', 'imgs_for_readme_rsz.zip')
+    # write_to_bucket('capstonedatajen', '../imgs_for_readme_rsz.zip', 'imgs_for_readme_rsz.zip')
     # write_to_bucket('capstonedatajen', '../imagemagick-identify-parser.zip', 'imagemagick-identify-parser')
+    write_folder_to_bucket('../misclass_imgs/')

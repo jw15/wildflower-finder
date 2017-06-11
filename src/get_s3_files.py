3befea110bacfc26282f9d4b391878c774fcdb02
JWaller <wallerj@gmail.com>
2017-06-11 15:07:53 -0600

    updated var names

--- src/get_s3_files.py
+++ src/get_s3_files.py
@@ -24,19 +24,21 @@ def retrieve_from_bucket(file):
 
 def write_to_bucket(bucket_name, filepath, filename):
     s3 = boto3.client('s3')
+    print('Uploading {}...'.format(filename))
     s3.upload_file(filepath, bucket_name, filename)
-    print('File uploaded - yay!')
+    print('  {} uploaded - yay!'.format(filename))
 
-def write_folder_to_bucket(root):
+def write_folder_to_bucket(root, bucket_name):
     s3 = boto3.client('s3')
     files = [f for f in listdir(root) if isfile(join(root, f))]
     for thing in files:
-        write_to_bucket('capstonedatajen', '{}{}'.format(root, thing), '{}'.format(thing))
-    print('Folder uploaded - HUZZAH!')
+        print('Uploading {}...'.format(thing))
+        write_to_bucket(bucket_name, '{}{}'.format(root, thing), '{}'.format(thing))
+    print('  {} uploaded - HUZZAH!'.format(root))
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # retrieve_from_bucket('imgs_jpgs.zip')
     # write_to_bucket('capstonedatajen', '../imgs_for_readme_rsz.zip', 'imgs_for_readme_rsz.zip')
-    write_to_bucket('capstonedatajen', 'resnet-101.t7', 'resnet-101.t7')
+    # write_to_bucket('capstonedatajen', '../model_plots/test.txt', 'test.txt')
     # write_folder_to_bucket('../misclass_imgs/')

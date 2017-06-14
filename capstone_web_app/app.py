bb7e3b24886d1686d60185327e48b892c10c54f9
JWaller <wallerj@gmail.com>
2017-06-13 21:52:53 -0600

    updates

--- capstone_web_app/app.py
+++ capstone_web_app/app.py
@@ -7,16 +7,19 @@ import os, sys
 from os import listdir
 from os.path import isfile, join
 from werkzeug import secure_filename
-import pickle
-
-import numpy as np
+import pickle, theano, pandas as pd, numpy as np
+from keras import optimizers
 from keras.models import load_model, model_from_json
+import os
+# print os.environ['DISPLAY']
 
 
 sys.path.insert(0, '../src')
 from img_preprocess_web import process_image
 
 sys.setrecursionlimit(1000000)
+
+# os.environ["THEANO_FLAGS"] = "cxx=''"
 
 
 # os.environ["THEANO_FLAGS"] = "device=cuda0"
@@ -51,6 +54,12 @@ def allowed_file(filename):
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/score', methods=['GET', 'POST'])
+# def load_model():
+#     model =  model_from_json(open('../model_outputs/ResNet50_1497216607_2807329/model.json').read())
+#     model.load_weights('../model_outputs/ResNet50_1497216607_2807329/ResNet50_1497216607_2807329.h5')
+#     model.compile(optimizer='sgd', loss='categorical_crossentropy')
+#     return model
+
 def score():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -68,30 +77,38 @@ def score():
             file.save(file_path)
             prepared_image = process_image('../{}'.format(file_path))
             # add something here to check if file is an image array/ check if RGB
-            prediction = loaded_model.predict(prepared_image)
+            prediction = model.predict(prepared_image)
             top_prediction = np.argmax(prediction)
             top_species = cats[top_prediction]
             # if top_species == 'delphinium nuttalianum':
             #     top_species = 'mertensia lanceolata'
-            # order = np.argsort(prediction)
+            order = np.argsort(prediction)
             # top_three = order[-3:]
             # top_three = top_three.tolist()
-            # top_five = order[-5:]
-            # top_five = top_five.tolist()
+            top_five = order[-5:]
+            top_five = top_five.tolist()
             # top_five_list = []
-            # for result in top_five:
-            #     species = cats[result]
-            #     top_five_list.append(species)
-            # top_five_list = top_five_list[::-1]
+            for result in top_five:
+                result = int(result)
+                species = cats[result]
+                top_five_list.append(species)
+            top_five_list = top_five_list[::-1]
             # print(top_species)
             # print(top_five_list)
-    return render_template('score.html', data=[top_species, top_prediction])
+    return render_template('score.html', data=[top_species, top_prediction, top_five_list])
 
 
 
 if __name__ == '__main__':
     print('Loading model...')
-    loaded_model = load_model('../model_outputs/ResNet50_1497216607_2807329/ResNet50_1497216607_2807329.h5')
+    # sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+    model = load_model('../model_outputs/ResNet50_1497216607_2807329/ResNet50_1497216607_2807329.h5')
+
+    #  model_from_json(open('../model_outputs/ResNet50_1497216607_2807329/model.json').read())
+    # model.load_weights('../model_outputs/ResNet50_1497216607_2807329/ResNet50_1497216607_2807329.h5')
+    # model.compile(optimizer=sgd, loss='categorical_crossentropy')
+    # model = load_model()
+    # loaded_model = load_model('../model_outputs/ResNet50_1497216607_2807329/ResNet50_1497216607_2807329.h5')
     # # load json and create model
     # json_file = open('../model_outputs/model.json', 'r')
     # loaded_model_json = json_file.read()
@@ -100,8 +117,10 @@ if __name__ == '__main__':
     # # load weights into new model
     # loaded_model.load_weights("../model_outputs/ResNet50_1497216607_2807329.h5")
     print("Loaded model from disk")
-    flower_cats = np.load('../model_outputs/ResNet50_1497216607_2807329/flower_count_df.pkl', allow_pickle=True)
-    cats = list(flower_cats.index)
+    flower_cats = np.load('classes.npy')
+    # flower_cats = np.loadtxt('../model_outputs/ResNet50_1497216607_2807329/flower_count_df.pkl', delimiter=',')
+    # print(type(flower_cats))
+    cats = flower_cats.tolist()
     print('Running app')
     app.run(host='0.0.0.0', port=8105, threaded=True, debug=True)
     # flowermap = download_file('flowermap.html')
